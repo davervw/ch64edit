4945c6474b750f69578e0621701684430ed59724
Dave Van Wagner <git@davevw.com>
2024-04-16 18:22:49 -0700

    refactor build script, long listing of files on disk

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 export ACME=${USERPROFILE}/Downloads/acme0.97win/acme
 export VICE=${USERPROFILE}/Downloads/GTK3VICE-3.8-win64/bin
 mkdir build 2>/dev/null
-${ACME}/acme -f cbm -o build/ch20edit.prg -l build/ch20edit.lbl ch20edit.asm
-[ $? -eq 0 ] && ${VICE}/c1541 ch20edit.d64 -attach ch20edit.d64 8 -delete ch20edit.prg -write build/ch20edit.prg
-[ $? -eq 0 ] && rm d64_files/*
-[ $? -eq 0 ] && ${VICE}/c1541 ch20edit.d64 -attach ch20edit.d64 8 -cd d64_files -extract
-[ $? -eq 0 ] && ${VICE}/xvic -moncommands build/ch20edit.lbl -autostart ch20edit.d64 >/dev/null 2>&1 &
+${ACME}/acme -f cbm -o build/ch20edit.prg -l build/ch20edit.lbl ch20edit.asm \
+&& ${VICE}/c1541 ch20edit.d64 -attach ch20edit.d64 8 -delete ch20edit.prg -write build/ch20edit.prg \
+&& rm d64_files/* \
+&& ${VICE}/c1541 ch20edit.d64 -attach ch20edit.d64 8 -cd d64_files -extract \
+&& ls -l d64_files \
+&& ${VICE}/xvic -moncommands build/ch20edit.lbl -autostart ch20edit.d64 >/dev/null 2>&1 &
