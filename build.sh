f6a16d570d338f08924aae28113a3383bc288129
Dave Van Wagner <git@davevw.com>
2024-04-20 11:23:30 -0700

    renames, label for 64

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 export ACME=${USERPROFILE}/Downloads/acme0.97win/acme
 export VICE=${USERPROFILE}/Downloads/GTK3VICE-3.8-win64/bin
 mkdir build 2>/dev/null
-${ACME}/acme -f cbm -o build/ch20edit.prg -l build/ch20edit.lbl ch20edit.asm \
-&& ${VICE}/c1541 ch20edit.d64 -attach ch20edit.d64 8 -delete ch20edit.prg -write build/ch20edit.prg \
+${ACME}/acme -f cbm -o build/ch64edit.prg -l build/ch64edit.lbl ch64edit.asm \
+&& ${VICE}/c1541 ch64edit.d64 -attach ch64edit.d64 8 -delete ch64edit.prg -write build/ch64edit.prg \
 && rm d64_files/* \
-&& ${VICE}/c1541 ch20edit.d64 -attach ch20edit.d64 8 -cd d64_files -extract \
+&& ${VICE}/c1541 ch64edit.d64 -attach ch64edit.d64 8 -cd d64_files -extract \
 && ls -l d64_files \
-&& ${VICE}/xvic -moncommands build/ch20edit.lbl -autostart ch20edit.d64 >/dev/null 2>&1 &
+&& ${VICE}/x64sc -moncommands build/ch64edit.lbl -autostart ch64edit.d64 >/dev/null 2>&1 &
