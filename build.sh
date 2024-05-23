9ee668f307811d8ae6e28ba7f78c4e1112e4d55b
Dave Van Wagner <git@davevw.com>
2024-05-22 21:50:25 -0700

    add report listing from build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 export ACME=${USERPROFILE}/Downloads/acme0.97win/acme
 export VICE=${USERPROFILE}/Downloads/GTK3VICE-3.8-win64/bin
 mkdir build 2>/dev/null
-${ACME}/acme -f cbm -o build/ch64edit.prg -l build/ch64edit.lbl ch64edit.asm \
+${ACME}/acme -f cbm -o build/ch64edit.prg -l build/ch64edit.lbl -r build/ch64edit.lst ch64edit.asm \
 && ${VICE}/c1541 ch64edit.d64 -attach ch64edit.d64 8 -delete ch64edit.prg -write build/ch64edit.prg \
 && rm d64_files/* \
 && ${VICE}/c1541 ch64edit.d64 -attach ch64edit.d64 8 -cd d64_files -extract \
